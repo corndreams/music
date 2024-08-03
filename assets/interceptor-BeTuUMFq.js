074b0a252fea595c43da505018ccd8f9cb45d4ef
corndreams <155290403+corndreams@users.noreply.github.com>
2024-08-03 17:08:21 +0800

    Add files via upload

---
+++ assets/interceptor-BeTuUMFq.js
@@ -0,0 +1,1 @@
+import{L as p,S as a}from"./index-DUQinITR.js";function m(l,{args:t=[],done:i,canceled:s,error:o}){if(l){const f=l.apply(null,t);p(f)?f.then(r=>{r?i():s&&s()}).catch(o||a):f?i():s&&s()}else i()}export{m as c};
